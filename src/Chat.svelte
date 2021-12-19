6fcf14cec12e85e6746cb6433072ddad29f92a0b
Jack Frain <jfrain99@gmail.com>
2021-12-19 09:46:53 -0500

    Initial Commit

---
+++ src/Chat.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Login from "./Login.svelte"
+    import { username } from "./user"
+</script>
+
+<div>
+    {#if $username}
+        <h1> Logged in </h1>
+    {:else}
+        <Login/>
+    {/if}
+</div>
---
+++ src/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { username, user } from "./user";
+    
+    function logout() {
+        user.leave()
+        username.set('')
+    }
+
+    console.log(username, user)
+</script>
+<header>
+    <h1>Jack Frain</h1>
+    {#if $username}
+        <span> Hello <strong>{$username}</strong></span>
+        <button on:click={logout}>
+            Log out
+        </button>
+    {:else}
+        <h1>Gunjs Chat</h1>
+    {/if}
+</header>
